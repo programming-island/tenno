16c5d78fb60695bdb493b515b6533aa02c190d4b
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2023-02-04 16:41:45 -0300

    homepage pronta

--- home/models.py
+++ home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class user(models.Model):
-    nome = models.CharField(max_length=20,black=True
+    nome = models.CharField(max_length=20,blank=True)
     sobrenome = models.CharField(max_length=20, blank=True)
     telefone = models.CharField(max_length=11 , blank = True)
     email = models.EmailField(max_length=50)
